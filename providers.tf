67ae5aad52f7c7f6482a349658f675925f42df0c
415212048 <34223550+415212048@users.noreply.github.com>
2023-08-21 14:40:20 +0800

    Add files via upload

--- providers.tf
+++ providers.tf
@@ -4,8 +4,8 @@ provider "huaweicloud" {
   access_key = "AYC7XMLT6UMNBABA03S3"
   secret_key = "ic249AA7uepl5hG4yrGp0q4W7dvckbDdYhREx8k1"
 
-  assume_role{
-    agency_name = "test_tansj"
-    domain_name = "hwid_7q8xoc99e68r3r1"
-  }
+#    assume_role{
+#      agency_name = "test_tansj"
+#      domain_name = "hwid_7q8xoc99e68r3r1"
+#    }
 }
